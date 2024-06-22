dfbb00b08fa7b16baac79915187bd4dee8036064
sampurna343 <sampurna343@gmail.com>
2024-06-22 23:42:08 +0530

    file read path
    Signed-off-by: sampurna343 <sampurna343@gmail.com>

--- SymptonToDiseasePrediction/data_preprocessing_tools.py
+++ SymptonToDiseasePrediction/data_preprocessing_tools.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Importing the dataset
-dataset = pd.read_csv('Data.csv')
+dataset = pd.read_csv('./Datasets/Symptoms to disease data/training.csv')
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
 print(X)
