b17e0d757b9995e076377623ccf8da9a91b46194
sampurna343 <sampurna343@gmail.com>
2024-07-13 17:44:19 +0530

    ui and git ignore
    Signed-off-by: sampurna343 <sampurna343@gmail.com>

--- ImageProcessingPOC/UploadImageUI.py
+++ ImageProcessingPOC/UploadImageUI.py
@@ -1,7 +1,15 @@
 import streamlit as st
+from PIL import Image
+import numpy as np
 
 st.header("Analyze Chest X Ray Plate")
 
 with st.sidebar:
     st.title("Images")
     file=st.file_uploader("Upload an inage to analyze",type="png")
+    if file is not None:
+        image = Image.open(file)
+        img_array = np.array(image)
+        image.show(file)
+
+
