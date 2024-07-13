4767b8762c0d036e1976743bc1db26f9715c7397
sampurna343 <sampurna343@gmail.com>
2024-07-14 05:17:02 +0530

    all image format
    Signed-off-by: sampurna343 <sampurna343@gmail.com>

--- ImageProcessingPOC/UploadImageUI.py
+++ ImageProcessingPOC/UploadImageUI.py
@@ -9,7 +9,7 @@ st.header("Analyze Chest X Ray Plate")
 image=[]
 with st.sidebar:
     st.title("Image Submit")
-    file=st.file_uploader("Upload an inage to analyze",type="png")
+    file=st.file_uploader("Upload an inage to analyze",type=["png","jpg","jpeg"])
     if file is not None:
         image = Image.open(file)
         img_array = np.array(image)
