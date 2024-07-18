a6174ef3e613370a0fbdbb8b5e3ede04d6d29210
sampurna343 <sampurna343@gmail.com>
2024-07-18 16:43:16 +0530

    ui
    Signed-off-by: sampurna343 <sampurna343@gmail.com>

--- DiseasePredictionByChestX-RayAnalysis/ImageUpload.py
+++ DiseasePredictionByChestX-RayAnalysis/ImageUpload.py
@@ -6,6 +6,9 @@ from ImageSubmit import submit
 
 #st.image("https://www.marnoa.ca/-/media/rj/rjl-advisor-sites/sites/m/a/marnoa/images/blogs/1-31-2024/danaher-thumbnail.jpg?h=238&w=800&la=en&hash=0C506AD8D9B97EF58ED2838CD0BA4BFB")
 st.image("c:\\Users\\SMAJUMDAR\\GITREPO\\NextGen-IoT\\src\\UI-Framework\\FE\\Common\\img\\BCI Logo.png")
+st.header("Image Processing of Chest X-Ray templates for Chest Disease prediction")
+
+st.image("https://sumas.edu.ng/wp-content/uploads/2023/02/skeleton-bg.jpg")
 st.header("Analyze Chest X Ray Plate")
 
 image=[]
